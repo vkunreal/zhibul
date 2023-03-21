d9ce31acee1fe3bc3215957c6beeb1d651094b78
vkunreal <75563669+vkunreal@users.noreply.github.com>
2023-03-21 12:48:12 +0300

    Added fixes

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -42,7 +42,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "http://194.67.110.169/api",
+    // baseURL: "http://194.67.110.169/api",
+    baseURL: "http://localhost:5000/api",
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
