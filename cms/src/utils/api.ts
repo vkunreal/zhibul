aeead1b7e2c7feef10dd84bb35c3319240b3532e
vkunreal <vkunr3al@gmail.com>
2023-09-18 20:45:57 +0300

    Fix domain

--- cms/src/utils/api.ts
+++ cms/src/utils/api.ts
@@ -2,6 +2,6 @@
 const API = 'https://api.zhbl.by'
 
 // export const DOMAIN = 'http://localhost:3000'
-export const DOMAIN = 'https://zhbl.by/'
+export const DOMAIN = 'https://zhbl.by'
 
 export default API
