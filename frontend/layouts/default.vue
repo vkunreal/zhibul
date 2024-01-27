71f46da3a50bf52475c01c470a27310b16ff2a89
vkunreal <vkunr3al@gmail.com>
2024-01-27 15:33:03 +0300

    Add chat

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -222,6 +222,18 @@ export default {
           return false;
         })
     );
+
+    (function (w, d, u) {
+      var s = d.createElement("script");
+      s.async = true;
+      s.src = u + "?" + ((Date.now() / 60000) | 0);
+      var h = d.getElementsByTagName("script")[0];
+      h.parentNode.insertBefore(s, h);
+    })(
+      window,
+      document,
+      "https://cdn-ru.bitrix24.by/b28268456/crm/site_button/loader_1_qhf2vf.js"
+    );
   },
   computed: {
     ...mapGetters("app", ["appVariables", "categories"]),
