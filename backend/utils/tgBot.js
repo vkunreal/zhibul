d8d0c041d3544c4defde758f6ce7351a386844ab
vkunreal <vkunr3al@gmail.com>
2024-07-07 02:07:04 +0300

    Add tg bot

--- backend/utils/tgBot.js
+++ backend/utils/tgBot.js
@@ -1,11 +1,15 @@
+const TelegramBot = require('node-telegram-bot-api')
+
+const bot = new TelegramBot(process.env.tg_bot_token)
+
 const sendRequestToBot = async ({ name, phone }) => {
   const userDataString = `
-    <b>Имя пользователя: </b> ${name}%0A%0A<b>Телефон: </b> ${phone}%0A
+    <b>Имя пользователя: </b> ${name}\n<b>Телефон: </b> ${phone}
   `
 
-  const requestString = `https://api.telegram.org/bot${process.env.tg_bot_token}/sendMessage?chat_id=${process.env.tg_bot_chat_id}&parse_mode=html&text=${userDataString}`
-
-  await fetch(requestString)
+  bot.sendMessage(process.env.tg_bot_chat_id, userDataString, {
+    parse_mode: 'HTML',
+  })
 }
 
 module.exports = { sendRequestToBot }
