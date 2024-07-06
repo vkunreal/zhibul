54b194023a84aaa5b520887bcd2441bb71295b2b
vkunreal <vkunr3al@gmail.com>
2024-07-07 01:37:20 +0300

    Fix url

--- backend/utils/tgBot.js
+++ backend/utils/tgBot.js
@@ -3,9 +3,9 @@ const sendRequestToBot = async ({ name, phone }) => {
     <b>Имя пользователя: </b> ${name}%0A%0A<b>Телефон: </b> ${phone}%0A
   `
 
-  await fetch(
-    `https://api.telegram.org/bot${process.env.tg_bot_token}/sendMessage?chat_id=${process.env.tg_bot_chat_id}&parse_mode=html&text=${userDataString}`
-  )
+  const requestString = `https://api.telegram.org/bot${process.env.tg_bot_token}/sendMessage?chat_id=${process.env.tg_bot_chat_id}&parse_mode=html&text=${userDataString}`
+
+  await fetch(requestString)
 }
 
 module.exports = { sendRequestToBot }
